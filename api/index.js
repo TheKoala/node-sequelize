69ca9fe68458803746f5d5ec85f5b8955e210217
Felipe Lima <felipelima8556@gmail.com>
2023-05-30 15:47:11 -0300

    install sequelize

--- api/index.js
+++ api/index.js
@@ -4,3 +4,15 @@ import bodyParser from "body-parser";
 const app = express();
 
 app.use(bodyParser.json());
+
+const port = 3000;
+
+app.get("/teste", (req, res) => {
+  res.status(200).send({ mensagem: "Hello World" });
+});
+
+app.listen(port, () =>
+  console.log(`servidor está rodando na porta http://localhost:${port}`)
+);
+
+export default app;
