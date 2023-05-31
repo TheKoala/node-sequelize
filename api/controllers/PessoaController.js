f0b9a7af570082c6b845884988c433ae1bebf7fc
Felipe Lima <felipelima8556@gmail.com>
2023-05-31 15:05:51 -0300

    create, update, delete and get for id Pessoas

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -2,10 +2,66 @@ const database = require("../models");
 
 class PessoaController {
   static listarPessoas(req, res) {
-    database.Pessoas
-      .findAll()
+    database.Pessoas.findAll()
       .then((pessoas) => {
         res.status(200).send(pessoas);
+      })
+      .catch((error) => {
+        res.status(500).send(error);
+      });
+  }
+
+  static listarPessoaPorId(req, res) {
+    const { id } = req.params;
+    database.Pessoas.findOne({
+      where: { id: Number(id) },
+    })
+      .then((pessoa) => {
+        res.status(200).send(pessoa);
+      })
+      .catch((error) => {
+        res.status(500).send(error);
+      });
+  }
+
+  static cadastraPessoa(req, res) {
+    const novaPessoa = req.body;
+
+    database.Pessoas.create(novaPessoa)
+      .then((pessoa) => {
+        res.status(200).send(pessoa);
+      })
+      .catch((error) => {
+        res.status(500).send(error);
+      });
+  }
+
+  static async atualizaPessoa(req, res) {
+    const { id } = req.params;
+    const pessoaAtual = req.body;
+
+    try {
+      await database.Pessoas.update(pessoaAtual, {
+        where: { id: Number(id) },
+      });
+
+      database.Pessoas.findOne({
+        where: { id: Number(id) },
+      }).then((pessoa) => {
+        res.status(200).send(pessoa);
+      });
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  }
+
+  static deletaPessoa(req, res) {
+    const { id } = req.params;
+    database.Pessoas.destroy({
+      where: { id: Number(id) },
+    })
+      .then(() => {
+        res.status(200).send();
       })
       .catch((error) => {
         res.status(500).send(error);
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -3,6 +3,11 @@ const PessoaController = require("../controllers/PessoaController");
 
 const router = Router();
 
-router.get("/pessoas", PessoaController.listarPessoas);
+router
+  .get("/pessoas", PessoaController.listarPessoas)
+  .get("/pessoas/:id", PessoaController.listarPessoaPorId)
+  .post("/pessoas", PessoaController.cadastraPessoa)
+  .put("/pessoas/:id", PessoaController.atualizaPessoa)
+  .delete("/pessoas/:id", PessoaController.deletaPessoa);
 
 module.exports = router;
